eaf96d9e30f33defdd515fa72d172629803633ce
mornir <jeromepott@hotmail.com>
2020-02-06 19:21:00 +0100

    First commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: function(h) {
-    return h(App);
-  }
-}).$mount("#app");
+    return h(App)
+  },
+}).$mount('#app')
