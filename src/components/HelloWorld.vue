44962b6bab44196e46a8a2ab8b3923ba7bcfef28
mornir <jeromepott@hotmail.com>
2020-02-07 11:42:28 +0100

    Add tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@
         >vue-cli documentation</a
       >.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3 class="text-red-500">Installed CLI Plugins</h3>
     <ul>
       <li>
         <a
@@ -110,21 +110,3 @@ export default {
   },
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <button class="border-2 border-blue-500 px-2" @click="connect">
+      Connect with Pocket
+    </button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from 'axios'
+
+const REDIRECT_URI = 'http://localhost:8080/'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  methods: {
+    connect() {
+      axios
+        .post('https://getpocket.com/v3/oauth/request', {
+          consumer_key: process.env.VUE_APP_CONSUMER_KEY,
+          redirect_uri: REDIRECT_URI,
+        })
+        .then(response => {
+          const REQUEST_TOKEN = response.code
+          window.location.assign(
+            `https://getpocket.com/auth/authorize?request_token=${REQUEST_TOKEN}&redirect_uri=${REDIRECT_URI}`
+          )
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
   },
 }
 </script>
