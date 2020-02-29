d6f0fc1cc433bde58ed364f7dac73cfbe7ad6ff6
mornir <jeromepott@hotmail.com>
2020-02-29 15:17:13 +0100

    Add montserra font

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="p-4 font-sans">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,28 @@
 module.exports = {
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [
+        'Montserrat',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+    },
+    extend: {
+      colors: {
+        primary: '#EB5757',
+      },
+    },
   },
   variants: {},
   plugins: [],
