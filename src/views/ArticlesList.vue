4a666f7363c0f532b8562e57e38c2a122f621c98
mornir <jeromepott@hotmail.com>
2020-06-25 19:53:18 +0200

    Code cleanup

--- src/views/ArticlesList.vue
+++ src/views/ArticlesList.vue
@@ -54,14 +54,3 @@ export default {
   },
 }
 </script>
-
-<style lang="postcss" scoped>
-/* @sreen md {
-  .c-grid {
-    display: grid;
-    grid-auto-rows: 1fr;
-    grid-template-columns: repeat(auto-fit, minmax(5rem, 1fr));
-    grid-gap: 1rem;
-  }
-} */
-</style>
