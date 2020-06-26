0148be82cc291b7a500c5e342d0a71549adb2a30
mornir <jeromepott@hotmail.com>
2020-06-26 21:10:23 +0200

    Add text about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>Why Pocket Lite?</h1>
+    <p>
+      I made Pocket Lite because I realize that I was only using it a list of
+      links. I almost never use the Pocket Reader and instead open the link in
+      new tab for reading on the real website.
+    </p>
+    <p>
+      I also use Pocket at work, where Twitter is blocked. This means that
+      articles saved directly from Twitter with the Pocket integration can't be
+      opened by clicking on the subtitle link.
+    </p>
   </div>
 </template>
-
-<script>
-export default {
-  mounted() {
-    console.log(this.$route.query.token)
-  },
-}
-</script>
